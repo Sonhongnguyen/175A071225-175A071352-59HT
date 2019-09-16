21117c210c1767f16238616bece9d37ebc73f235
pmskipper99 <pmskipper99@gmail.com>
2019-09-16 22:51:59 +0700

    cập nhâp mới
    thêm sửa một chust

--- BTL2 web/js/scrip.js
+++ BTL2 web/js/scrip.js
@@ -1,23 +1,5 @@
-<script src="./Trường Đại Học Nguyễn Tất Thành - NTTU_files/e1e2699.js"></script>
-
-    <script>
-        $(document).ready(function() {
-            $("#slider-main").on("click", ".carousel-item", function() {
-                window.location = $(this).data('link');
-            });
-        });
-    </script>
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/58520b90651e34097abade60/default';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    </script>
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
